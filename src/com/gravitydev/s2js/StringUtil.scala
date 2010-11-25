bb51739e58e5720c8c688264338e8d46b7b10659
Alvaro Carrasco <alvaro@gravitydev.com>
2010-11-24 19:55:11 -0700

    Set up the basic sbt project structure (using Erick Flemings' fork as a rough guide), other minor improvements

--- src/com/gravitydev/s2js/StringUtil.scala
+++
@@ -1,5 +0,0 @@
-package com.gravitydev.s2js
-
-object StringUtil {
-	def indent (text:String) = text.split("\n").map("  "+_).mkString("\n") + "\n"
-}
--- src/com/gravitydev/s2js/sample/Test.scala
+++
@@ -1,7 +0,0 @@
-package gravity
-
-class Test {
-	def main () {
-		new goog.fx.Dragger(null, null)
-	}
-}
